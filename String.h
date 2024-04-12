08f3f1cd54ce463c7e22f230fab552539d494aea
= <=>
2024-04-13 00:04:51 +0700

    Commit

---
+++ String.h
@@ -0,0 +1,25 @@
+#ifndef STRING_H
+#define STRING_H
+
+int len(char *s);
+char *uppercase(char *s);
+bool isUpper(char *s);
+char *lowercase(char *s);
+bool isLower(char *s);
+bool compare(char *s1, char *s2);
+bool compareIgnoreCase(char *s1, char *s2);
+char *removeSpace(char *s);
+char *replace(char *s, char oldChar, char newChar);
+bool startsWith(char *s, char *start);
+bool startsWithIgnoreCase(char *s, char *start);
+bool endsWith(char *s, char *end);
+bool endsWithIgnoreCase(char *s, char *end);
+int indexOf(char *s, char target);
+int indexOfWithIgnoreCase(char *s, char target);
+char **split(char *s, char separator);
+bool isEmpty(char* s);
+char charAt(char* s,int index);
+char* stringRepeat(char* s , int repeat);
+void cleanMalloc();
+
+#endif
